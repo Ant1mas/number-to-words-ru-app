b6965f04f287317fa1b1ec658db2b8933a2eb6a4
Anton Moskovskiy <7397158+Ant1mas@users.noreply.github.com>
2022-05-15 12:02:21 +0500

    component "DeclensionsFields" refactored

--- src/components/DeclensionsFields.tsx
+++ src/components/DeclensionsFields.tsx
@@ -23,289 +23,106 @@ export function DeclensionsFields(props: props) {
   const { options, updateOptions } = useModuleOptions()
   const { t } = useI18n()
 
-  return (
-    <>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-nominative-singular`}
-          label={t(
-            'options_currency_custom_value_declension_nominative_singular'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].nominative[0]
-          }
-          value={options.customCurrency[declensionsObjectName].nominative[0]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color: options.declension === 'nominative' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'nominative'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-nominative-plural`}
-          label={t(
-            'options_currency_custom_value_declension_nominative_plural'
-          )}
-          variant="standard"
-          fullWidth
-          disabled
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].nominative[1]
-          }
-          value={t('options_currency_custom_value_declension_disabled')}
-          onChange={updateOptions}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-genitive-singular`}
-          label={t(
-            'options_currency_custom_value_declension_genitive_singular'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].genitive[0]
-          }
-          value={options.customCurrency[declensionsObjectName].genitive[0]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color:
-                options.declension === 'nominative' ||
-                options.declension === 'genitive' ||
-                options.declension === 'accusative'
-                  ? 'primary.main'
-                  : '',
-            },
-          }}
-          helperText={
-            options.declension === 'nominative' ||
-            options.declension === 'genitive' ||
-            options.declension === 'accusative'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-genitive-plural`}
-          label={t('options_currency_custom_value_declension_genitive_plural')}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].genitive[1]
-          }
-          value={options.customCurrency[declensionsObjectName].genitive[1]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color: 'primary.main',
-            },
-          }}
-          helperText={t('options_currency_custom_value_declension_used')}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-dative-singular`}
-          label={t('options_currency_custom_value_declension_dative_singular')}
-          variant="standard"
-          fullWidth
-          placeholder={DEFAULT_CURRENCY_OBJECT[declensionsObjectName].dative[0]}
-          value={options.customCurrency[declensionsObjectName].dative[0]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color: options.declension === 'dative' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'dative'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-dative-plural`}
-          label={t('options_currency_custom_value_declension_dative_plural')}
-          variant="standard"
-          fullWidth
-          placeholder={DEFAULT_CURRENCY_OBJECT[declensionsObjectName].dative[1]}
-          value={options.customCurrency[declensionsObjectName].dative[1]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color: options.declension === 'dative' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'dative'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-accusative-singular`}
-          label={t(
-            'options_currency_custom_value_declension_accusative_singular'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].accusative[0]
-          }
-          value={options.customCurrency[declensionsObjectName].accusative[0]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color: options.declension === 'accusative' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'accusative'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-accusative-plural`}
-          label={t(
-            'options_currency_custom_value_declension_accusative_plural'
-          )}
-          variant="standard"
-          fullWidth
-          disabled
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].accusative[1]
-          }
-          value={t('options_currency_custom_value_declension_disabled')}
-          onChange={updateOptions}
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-instrumental-singular`}
-          label={t(
-            'options_currency_custom_value_declension_instrumental_singular'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].instrumental[0]
-          }
-          value={options.customCurrency[declensionsObjectName].instrumental[0]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color:
-                options.declension === 'instrumental' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'instrumental'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-instrumental-plural`}
-          label={t(
-            'options_currency_custom_value_declension_instrumental_plural'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].instrumental[1]
-          }
-          value={options.customCurrency[declensionsObjectName].instrumental[1]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color:
-                options.declension === 'instrumental' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'instrumental'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-prepositional-singular`}
-          label={t(
-            'options_currency_custom_value_declension_prepositional_singular'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].prepositional[0]
-          }
-          value={options.customCurrency[declensionsObjectName].prepositional[0]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color:
-                options.declension === 'prepositional' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'prepositional'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <TextField
-          name={`custom-currency-${numberPart}-declension-prepositional-plural`}
-          label={t(
-            'options_currency_custom_value_declension_prepositional_plural'
-          )}
-          variant="standard"
-          fullWidth
-          placeholder={
-            DEFAULT_CURRENCY_OBJECT[declensionsObjectName].prepositional[1]
-          }
-          value={options.customCurrency[declensionsObjectName].prepositional[1]}
-          onChange={updateOptions}
-          sx={{
-            input: {
-              color:
-                options.declension === 'prepositional' ? 'primary.main' : '',
-            },
-          }}
-          helperText={
-            options.declension === 'prepositional'
-              ? t('options_currency_custom_value_declension_used')
-              : ''
-          }
-        />
-      </Grid>
-    </>
+  const declensions = Object.keys(
+    DEFAULT_CURRENCY_OBJECT.currencyNameDeclensions
   )
+  let fieldsObjects = []
+  declensions.forEach((declension) => {
+    fieldsObjects.push(
+      {
+        declension: declension,
+        form: 'singular',
+      },
+      {
+        declension: declension,
+        form: 'plural',
+      }
+    )
+  })
+  const fieldsJSX = fieldsObjects.map((fieldObject) => {
+    const disabled =
+      (fieldObject.declension === 'nominative' &&
+        fieldObject.form === 'plural') ||
+      (fieldObject.declension === 'accusative' && fieldObject.form === 'plural')
+        ? true
+        : false
+    let value =
+      options.customCurrency[declensionsObjectName][fieldObject.declension][
+        fieldObject.form === 'singular' ? 0 : 1
+      ]
+    if (disabled) {
+      value = t('options_currency_custom_value_declension_disabled')
+    }
+    let selected = false
+    if (
+      fieldObject.declension === 'nominative' &&
+      fieldObject.form === 'singular'
+    ) {
+      selected = options.declension === 'nominative' ? true : false
+    } else if (
+      fieldObject.declension === 'genitive' &&
+      fieldObject.form === 'singular'
+    ) {
+      selected =
+        options.declension === 'nominative' ||
+        options.declension === 'genitive' ||
+        options.declension === 'accusative'
+          ? true
+          : false
+    } else if (
+      fieldObject.declension === 'genitive' &&
+      fieldObject.form === 'plural'
+    ) {
+      selected = true
+    } else if (fieldObject.declension === 'dative') {
+      selected = options.declension === 'dative' ? true : false
+    } else if (
+      fieldObject.declension === 'accusative' &&
+      fieldObject.form === 'singular'
+    ) {
+      selected = options.declension === 'accusative' ? true : false
+    } else if (fieldObject.declension === 'instrumental') {
+      selected = options.declension === 'instrumental' ? true : false
+    } else if (fieldObject.declension === 'prepositional') {
+      selected = options.declension === 'prepositional' ? true : false
+    }
+
+    return (
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        key={`${fieldObject.declension}-${fieldObject.form}`}
+      >
+        <TextField
+          name={`custom-currency-${numberPart}-declension-${fieldObject.declension}-${fieldObject.form}`}
+          label={t(
+            `options_currency_custom_value_declension_${fieldObject.declension}_${fieldObject.form}`
+          )}
+          variant="standard"
+          fullWidth
+          disabled={disabled}
+          placeholder={
+            DEFAULT_CURRENCY_OBJECT[declensionsObjectName][
+              fieldObject.declension
+            ][fieldObject.form === 'singular' ? 0 : 1]
+          }
+          value={value}
+          onChange={updateOptions}
+          sx={{
+            input: {
+              color: selected ? 'primary.main' : '',
+            },
+          }}
+          helperText={
+            selected ? t('options_currency_custom_value_declension_used') : ''
+          }
+        />
+      </Grid>
+    )
+  })
+
+  return <>{fieldsJSX}</>
 }
 
 DeclensionsFields.propTypes = {
