dbbc6ecea09f4da62c3635744720cd2fd5897393
Anton Moskovskii <ant1mas.dev@gmail.com>
2023-04-10 14:37:31 +0500

    Add logo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
-import Link from '@mui/material/Link'
 
 import useI18n from 'lib/hooks/useI18n'
+import LogoAnt1mas from 'public/images/svg/logo-ant1mas.svg'
 
 export default function Footer() {
   const { t } = useI18n()
@@ -11,7 +11,8 @@ export default function Footer() {
     <Grid
       container
       component="footer"
-      style={{ paddingBottom: '2rem', textAlign: 'center' }}
+      style={{ textAlign: 'center' }}
+      className="my-8"
     >
       <Grid item xs={12}>
         <Typography variant="caption" display="block">
@@ -23,23 +24,14 @@ export default function Footer() {
           >
             <strong>number-to-words-ru</strong>
           </a>{' '}
-          (2.3.4)
+          (v2.3.4)
         </Typography>
-        <Typography variant="caption" display="block">
-          © {new Date().getFullYear()} {t('author_name_anton_moskovskiy')}
-        </Typography>
-        <Typography variant="caption" display="block">
-          {t('made_by_project') + ' '}
-          <Link
-            href="https://github.com/Ant1mas"
-            target="_blank"
-            rel="noreferrer"
-            underline="always"
-            variant="caption"
-          >
-            {t('author_name_anton_moskovskiy')}
-          </Link>
-        </Typography>
+        <div className="mt-2 w-full font-sans">{t('made_by_project')}</div>
+        <div className="my-1 flex w-full justify-center">
+          <a href="https://github.com/Ant1mas" target="_blank" rel="noreferrer">
+            <LogoAnt1mas className="h-16 cursor-pointer fill-black opacity-70 duration-150 hover:opacity-100 focus:opacity-100" />
+          </a>
+        </div>
       </Grid>
     </Grid>
   )
