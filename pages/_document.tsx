8484c279f67187bbbedf3b1a721fa7dcb7e1e8f3
Anton Moskovskii <ant1mas.dev@gmail.com>
2023-04-14 11:54:34 +0500

    Update lang of html element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,18 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import clsx from 'clsx'
 
 import { roboto } from 'lib/config/fonts'
+import i18nextConfig from '@/next-i18next.config'
 
 export default class MyDocument extends Document {
   render() {
+    const currentLocale =
+      this.props.__NEXT_DATA__.locale ?? i18nextConfig.i18n.defaultLocale
     return (
-      <Html lang="en" className={clsx([roboto.variable])} data-theme="light">
+      <Html
+        lang={currentLocale}
+        className={clsx([roboto.variable])}
+        data-theme="light"
+      >
         <Head />
         <body>
           <Main />
