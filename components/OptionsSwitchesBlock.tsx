ec1e24536a1a627ee64e6f18b5a4dc6dacfccc9b
Anton Moskovskii <ant1mas.dev@gmail.com>
2023-10-04 10:35:12 +0500

    Update options switches

--- components/OptionsSwitchesBlock.tsx
+++ components/OptionsSwitchesBlock.tsx
@@ -1,6 +1,7 @@
+import { Switch } from '@nextui-org/switch'
+
 import useModuleOptions from 'lib/config/redux/slices/moduleOptions/useModuleOptions'
 import useI18n from 'lib/hooks/useI18n'
-import InputSwitch from 'components/InputSwitch'
 
 export default function OptionsSwitchesBlock() {
   const { options, updateOptions } = useModuleOptions()
@@ -9,60 +10,67 @@ export default function OptionsSwitchesBlock() {
   return (
     <>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="convert-minus-sign"
-          label={t('options_convert_minus_sign')}
-          checked={options.convertMinusSignToWord}
+          isSelected={options.convertMinusSignToWord}
           onChange={updateOptions}
-        />
+        >
+          {t('options_convert_minus_sign')}
+        </Switch>
       </div>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="show-number-part-integer"
-          label={t('options_show_integer_part')}
-          checked={options.showNumberParts.integer}
+          isSelected={options.showNumberParts.integer}
           onChange={updateOptions}
-        />
+        >
+          {t('options_show_integer_part')}
+        </Switch>
       </div>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="show-number-part-fractional"
-          label={t('options_show_fractional_part')}
-          checked={options.showNumberParts.fractional}
+          isSelected={options.showNumberParts.fractional}
           onChange={updateOptions}
-        />
+        >
+          {t('options_show_fractional_part')}
+        </Switch>
       </div>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="convert-number-part-integer"
-          label={t('options_convert_integer_part')}
-          checked={options.convertNumberToWords.integer}
+          isSelected={options.convertNumberToWords.integer}
           onChange={updateOptions}
-        />
+        >
+          {t('options_convert_integer_part')}
+        </Switch>
       </div>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="convert-number-part-fractional"
-          label={t('options_convert_fractional_part')}
-          checked={options.convertNumberToWords.fractional}
+          isSelected={options.convertNumberToWords.fractional}
           onChange={updateOptions}
-        />
+        >
+          {t('options_convert_fractional_part')}
+        </Switch>
       </div>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="show-currency-part-integer"
-          label={t('options_show_currency_integer_part')}
-          checked={options.showCurrency.integer}
+          isSelected={options.showCurrency.integer}
           onChange={updateOptions}
-        />
+        >
+          {t('options_show_currency_integer_part')}
+        </Switch>
       </div>
       <div className="p-1">
-        <InputSwitch
+        <Switch
           name="show-currency-part-fractional"
-          label={t('options_show_currency_fractional_part')}
-          checked={options.showCurrency.fractional}
+          isSelected={options.showCurrency.fractional}
           onChange={updateOptions}
-        />
+        >
+          {t('options_show_currency_fractional_part')}
+        </Switch>
       </div>
     </>
   )
