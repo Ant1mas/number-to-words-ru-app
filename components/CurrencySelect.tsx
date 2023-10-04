1dfa6a20b72dab4752ae78b27f10c0164ab3aad7
Anton Moskovskii <ant1mas.dev@gmail.com>
2023-10-04 09:37:23 +0500

    Fix option currency

--- components/CurrencySelect.tsx
+++ components/CurrencySelect.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { Select, SelectItem } from '@nextui-org/select'
 
 import useModuleOptions from 'lib/config/redux/slices/moduleOptions/useModuleOptions'
@@ -7,21 +6,19 @@ import useI18n from 'lib/hooks/useI18n'
 export default function CurrencySelect() {
   const { options, updateOptions } = useModuleOptions()
   const { t } = useI18n()
-  const currencies = useMemo(() => {
-    return [
-      { value: 'rub', label: t('options_currency_select_value_rub', '...') },
-      { value: 'usd', label: t('options_currency_select_value_usd', '...') },
-      { value: 'eur', label: t('options_currency_select_value_eur', '...') },
-      {
-        value: 'number',
-        label: t('options_currency_select_value_number', '...'),
-      },
-      {
-        value: 'custom',
-        label: t('options_currency_select_value_custom', '...'),
-      },
-    ]
-  }, [])
+  const currencies = [
+    { value: 'rub', label: t('options_currency_select_value_rub', '...') },
+    { value: 'usd', label: t('options_currency_select_value_usd', '...') },
+    { value: 'eur', label: t('options_currency_select_value_eur', '...') },
+    {
+      value: 'number',
+      label: t('options_currency_select_value_number', '...'),
+    },
+    {
+      value: 'custom',
+      label: t('options_currency_select_value_custom', '...'),
+    },
+  ]
 
   return (
     <Select
@@ -33,7 +30,6 @@ export default function CurrencySelect() {
       className="w-full"
       onSelectionChange={(selected: any) => {
         selected.forEach((element) => {
-          console.log('element', element)
           updateOptions({
             target: { name: 'currency', value: element, type: 'text' },
           })
